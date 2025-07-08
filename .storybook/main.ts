304efed41328a4d59ffb71da8dce47b6ae2aea67
xiangying <248619945@qq.com>
2025-07-08 15:40:03 +0800

    chore: 初始化Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,11 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-a11y", "@storybook/addon-vitest"],
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
+  },
+};
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import storybook from "eslint-plugin-storybook";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +14,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintConfigPrettier,
+  ...storybook.configs["flat/recommended"],
   {
     rules: {
       "import/no-anonymous-default-export": "off",
